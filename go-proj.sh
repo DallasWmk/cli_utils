3b66dcbc07b1f951373a49efdee061627afb0139
Dallas Womack <dallasawomack@gmail.com>
2025-07-07 11:18:25 -0700

    feat(go-proj):add util for quickly setting up a golang project

---
+++ go-proj.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+APP=$(gum input --placeholder "app name")
+CWD=$(pwd)
+mkdir "$CWD/$APP" || exit 1
+mkdir "$CWD/$APP/cmd" || exit 1
+cd "$CWD/$APP/cmd" || exit 1
+go mod init github.com/DallasWmk/"$APP"
+cd ..
+touch "$CWD/$APP/cmd/main.go"
+
+mkdir "$CWD/$APP/internal"
+mkdir "$CWD/$APP/internal/$APP"
+mkdir "$CWD/$APP/internal/config"
+
+mkdir "$CWD/$APP/pkg"
